392c840ae4870df1e7f1d8a44f1d6a8b2c216ef8
root <18205607106@163.com>
2017-07-21 00:08:37 +0800

    second version

---
+++ reactor/s13/HttpServer.h
@@ -0,0 +1,59 @@
+#ifndef MUDUO_NET_HTTP_HTTPSERVER_H
+#define MUDUO_NET_HTTP_HTTPSERVER_H
+
+#include "TcpServer.h"
+#include <boost/noncopyable.hpp>
+#include "HttpRequest.h"
+#include "HttpResponse.h"
+
+namespace muduo
+{
+
+//class HttpRequest;
+//class HttpResponse;
+
+/// A simple embeddable HTTP server designed for report status of a program.
+/// It is not a fully HTTP 1.1 compliant server, but provides minimum features
+/// that can communicate with HttpClient and Web browser.
+/// It is synchronous, just like Java Servlet.
+class HttpServer : boost::noncopyable
+{
+ public:
+  typedef boost::function<void (const HttpRequest&,
+                                HttpResponse*)> HttpCallback;
+
+  HttpServer(EventLoop* loop,
+             const InetAddress& listenAddr);
+
+  ~HttpServer();  // force out-line dtor, for scoped_ptr members.
+
+  EventLoop* getLoop() const { return server_.getloop(); }
+
+  /// Not thread safe, callback be registered before calling start().
+  void setHttpCallback(const HttpCallback& cb)
+  {
+    httpCallback_ = cb;
+  }
+
+  void setThreadNum(int numThreads)
+  {
+    server_.setThreadNum(numThreads);
+  }
+
+  void start();
+
+ private:
+  void onConnection(const TcpConnectionPtr& conn);
+  void onMessage(const TcpConnectionPtr& conn,
+                 Buffer* buf,
+                 Timestamp receiveTime);
+  void onRequest(const TcpConnectionPtr&, const HttpRequest&);
+
+  TcpServer server_;
+  HttpCallback httpCallback_;
+};
+
+
+}
+
+#endif  // MUDUO_NET_HTTP_HTTPSERVER_H
